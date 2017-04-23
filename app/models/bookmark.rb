63554244e4003348d179decef300710684f32a4e
Jakub Płoskonka <ploskonkajakub@gmail.com>
2017-04-23 19:16:51 +0200

    validate bookmark URL

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,4 @@
 class Bookmark < ApplicationRecord
   validates :url, :title, presence: true
+  validates :url, url: true
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 describe Bookmark do
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:url) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:url) }
+
+    describe 'url validation' do
+      context 'with incorrect url' do
+        subject { build(:bookmark, url: 'this-is-not-url') }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context 'with correct url' do
+        subject { build(:bookmark) }
+
+        it { is_expected.to be_valid }
+      end
+    end
+  end
 end
